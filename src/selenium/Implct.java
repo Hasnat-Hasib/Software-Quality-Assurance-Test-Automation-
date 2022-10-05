88856a745a7bc0878ab2c0ae3c6f5767c05a9881
unknown <hasib8nh@gmail.com>
2022-10-05 21:16:59 +0600

    Implicit/Explicit

--- src/selenium/Implct.java
+++ src/selenium/Implct.java
@@ -1,5 +1,5 @@
-package Tests;
-
+package selenium;
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -9,28 +9,26 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public class synchroniz {
+public class Implct {
+    public static WebDriver driver;
 
+    Duration timeout;
     public static void main(String[] args) throws InterruptedException {
 // TODO Auto-generated method stub
-        System.setProperty("webdriver.chrome.driver","C:\\work\\chromedriver.exe");
-        WebDriver driver=new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "C:\\work\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
 // driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.get("https://alaskatrips.poweredbygps.com/g/pt/hotels?MDPCID=ALASKA-US.TPS.BRAND.hotels.HOTEL");
         driver.findElement(By.id("H-destination")).sendKeys("nyc");
         driver.findElement(By.id("H-destination")).sendKeys(Keys.TAB);
         driver.findElement(By.id("H-fromDate")).sendKeys(Keys.ENTER);
 
-        WebDriverWait d=new WebDriverWait(driver,20);
+        WebDriverWait d = new WebDriverWait(driver, Duration.ofSeconds(10));
         d.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@id='resultsContainer']/section/article[1]")));
 //Thread.sleep(5000L);
 
         driver.findElement(By.xpath("//div[@id='resultsContainer']/section/article[1]")).click();
 
 
-
-
-
     }
-
 }
