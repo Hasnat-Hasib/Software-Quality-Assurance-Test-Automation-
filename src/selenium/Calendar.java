c6b459931a90df232e036734e1a317a12457b25c
unknown <hasib8nh@gmail.com>
2022-10-07 13:57:20 +0600

    Auto suggest drop down

--- src/selenium/Calendar.java
+++ src/selenium/Calendar.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Calendar {
-    public static WebDriver driver;
+    public static WebDriver obj;
     public static void main(String[] args) throws InterruptedException {
 
 
